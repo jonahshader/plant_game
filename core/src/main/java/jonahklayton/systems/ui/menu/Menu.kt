d740df1312c880445272646906b0897336874f0b
Jonah Shader <jonah.shader@gmail.com>
2021-05-01 02:27:57 -0400

    menu centered

--- core/src/main/java/jonahklayton/systems/ui/menu/Menu.kt
+++ core/src/main/java/jonahklayton/systems/ui/menu/Menu.kt
@@ -10,10 +10,9 @@ import space.earlygrey.shapedrawer.ShapeDrawer
 class Menu(private val font: TextRenderer.Font, private val camera: Camera, private val position: Vector2, private val menuItemSize: Vector2) {
     private val items = ArrayList<MenuItem>()
     fun addMenuItem(label: String, action: () -> Unit) {
-        // TODO: add menu item to items arraylist
         val newItem: MenuItem = if (items.size == 0) {
             //Call first menu item constructor
-            MenuItem(action, -menuItemSize.x/2f, 0f, menuItemSize.x, menuItemSize.y, label, font, camera)
+            MenuItem(action, 0f, 0f, menuItemSize.x, menuItemSize.y, label, font, camera)
         } else {
             //Call other items constructor
             MenuItem(action, label, items[items.size - 1], font, camera)
